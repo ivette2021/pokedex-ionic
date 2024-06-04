3fc5c81b2337b017b4ac99a04fee5ed40967c8f3
iconsWork <iconstanzav1@gmail.com>
2024-06-04 17:56:31 -0400

    rebase project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '04_pokedex',
-  webDir: 'www'
+  appName: 'Pokedex',
+  webDir: 'www',
 };
 
 export default config;
---
+++ src/app/pages/list-pokemons/list-pokemons.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-list-pokemons',
+  templateUrl: './list-pokemons.page.html',
+  styleUrls: ['./list-pokemons.page.scss'],
+})
+export class ListPokemonsPage implements OnInit {
+
+  public pokemons: Pokemon[];
+
+  constructor(
+    private pokemonService: PokemonService
+  ) { 
+    this.pokemons = [];
+  }
+
+  ngOnInit() {
+    this.morePokemon();
+  }
+
+  morePokemon(){
+
+    const promise = this.pokemonService.getPokemons();
+
+    if(promise){
+
+      promise.then( (result: Pokemon[]) => {
+
+        console.log(result);
+        
+        this.pokemons = this.pokemons.concat(result);
+
+        console.log(this.pokemons);
+      });
+
+    }
+
+
+  }
+
+}
